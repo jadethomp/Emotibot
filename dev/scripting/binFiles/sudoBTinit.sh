390c302176192ae96c8e0216ddbfaed375c5febc
Jade Thompson <jadethompson285@gmail.com>
2024-03-06 17:49:44 -0600

    oops replaced sudoBT with correct one

--- dev/scripting/binFiles/sudoBTinit.sh
+++ dev/scripting/binFiles/sudoBTinit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo rfcomm bind 0 B4:E6:2D:DA:52:C3
-sudo rfcomm bind 1 B4:E6:2D:D9:F1:6F
+sudo rfcomm bind 0 B4:E6:2D:D9:F1:6F
+sudo rfcomm bind 1 B4:E6:2D:DA:52:C3
 sleep 5
 
 echo "BTSerial Ports Open on rfcomm0 and rfcomm1"
