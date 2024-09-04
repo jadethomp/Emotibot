5999c9a59f21f4548fc6fd0d549853d0a5cb62cd
jytole <kylerssmith@hotmail.com>
2024-09-04 10:03:40 -0500

    Added serial test files that I found on my mac

---
+++ dev/bluetooth/2testSerial.py
@@ -0,0 +1,20 @@
+import serial
+ser = serial.Serial("/dev/rfcomm0")
+
+# Send character 'S' to start the program
+ser.write(bytearray('S','ascii'))
+
+# Read line
+while True:
+	# Temporary Command Line Interface for Testing
+	userIn = input("Input Command (emotions h, s, a; quit with q): ")
+	if userIn == "q":
+		break
+	elif userIn == "h":
+		ser.write(b'h')
+	elif userIn == "s":
+		ser.write(b's')
+	elif userIn == "a":
+		ser.write(b'a')
+	while ser.in_waiting:
+		print(ser.read())
